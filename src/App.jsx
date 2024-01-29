58da72e9caacdfbde61d62cff4f5f9dc4992bd9b
lcslago <lcslago97@gmail.com>
2024-01-29 17:17:41 -0300

    refactor: a conexão com firebase foi delegada ao Context API

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import GlobalStyle from "./components/GlobalStyles"
 import Header from "./components/Header"
 import MessageBox from "./components/MessageBox"
 import OptionsList from "./components/OptionsList"
-import { atkn, gDt, gQaDb } from "./firebase/api/main"
-import { useEffect, useState } from "react"
+import { DatabaseProvider } from "./context/ApiContext"
 
 const Background = styled.div`
   background-color: #313E51;
   font-family: 'Rubik', sans-serif;
   color: white;
-  background-image: url('../../public/pattern-background-desktop-dark.svg');
+  background-image: url('/pattern-background-desktop-dark.svg');
   background-repeat: no-repeat;
   background-size: 100vw;
   min-height: 100vh;
@@ -22,25 +21,15 @@ const Main = styled.main`
 `
 
 const App = () => {
-
-  const [data, setData] = useState([])
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    gQaDb()
-      .then(async () => setData(await gDt(atkn())))
-      .then(() => setLoading(false))
-  }, [])
-
-  !loading && console.log(data[0].title)
-
   return (
     <Background>
       <GlobalStyle />
       <Header></Header>
       <Main>
-        <MessageBox content="WelcomeMessage"></MessageBox>
-        <OptionsList></OptionsList>
+        <DatabaseProvider>
+          <MessageBox content="WelcomeMessage"></MessageBox>
+          <OptionsList></OptionsList>
+        </DatabaseProvider>
       </Main>
     </Background >
   )
--- src/components/OptionsList/index.jsx
+++ src/components/OptionsList/index.jsx
@@ -1,13 +1,26 @@
+import { useContext } from "react"
 import styled from "styled-components"
+import { ApiContext } from "../../context/ApiContext"
 
 const OptionsListContainer = styled.section`
     width: 50%;
     height: auto;
 `
 
-const OptionsList = () =>
-    <OptionsListContainer>
-        <span>OptionsList</span>
-    </OptionsListContainer>
+//CRIAR UMA ANIMAÇÃO DE LOADING ATÉ A RENDERIZAÇÃO DA LISTA DE CATEGORIAS
+
+const OptionsList = () => {
+    const { data, loading } = useContext(ApiContext);
+
+    return (
+        <OptionsListContainer>
+            <ul>
+                {!loading && data
+                    .map(category =>
+                        <li> {category.title} </li>)}
+            </ul>
+        </OptionsListContainer>
+    )
+}
 
 export default OptionsList
