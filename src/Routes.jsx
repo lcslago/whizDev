2d9de439a94c37f0b763148a29c73409ee83960a
lcslago <lcslago97@gmail.com>
2024-02-13 21:36:57 -0300

    refactor: mudança nas regras de acesso às rotas

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,10 +1,25 @@
-import { Route, Routes } from "react-router-dom";
-import { DefaultUI } from "./pages/Default";
+import { Route, Routes, useNavigate, useNavigationType } from "react-router-dom";
+import { Homepage } from "./pages/Homepage";
+import { useContext, useEffect } from "react";
+import { ApiContext } from "./context/ApiContext";
+import { Quiz } from "./pages/Quiz";
 
 export const AppRoutes = () => {
+    const { data, loading } = useContext(ApiContext)
+
+    const navigate = useNavigate()
+    const navigationType = useNavigationType()
+
+    useEffect(() => { navigationType === "POP" && navigate("/", { replace: true }) })
 
     return (
         <Routes>
-            <Route path="/" element={<DefaultUI />} />
+            <Route path="/" element={<Homepage />} />
+
+            {!loading && data.map(category =>
+                <Route
+                    path={`/${category.title.toLowerCase()}`}
+                    element={<Quiz />}
+                    key={category.id} />)}
         </Routes>)
 }
