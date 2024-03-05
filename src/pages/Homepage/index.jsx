71825951b9fbe83b60ff32379a0078730a204606
lcslago <lcslago97@gmail.com>
2024-03-05 11:16:59 -0300

    refactor: mudança de proposito para o componente Message

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -2,7 +2,7 @@ import { MessageBox } from "../../components/MessageBox";
 import { OptionsList } from "../../components/OptionsList";
 import { Header, HeaderTitle } from "../../components/Header";
 import { Main } from "../../App";
-import { WelcomeMessage } from "../../components/WelcomeMessage";
+import { Message } from "../../components/Message";
 import { CategoryList } from "../../components/CategoryList";
 
 
@@ -14,7 +14,16 @@ export const Homepage = () =>
 
         <Main>
             <MessageBox>
-                <WelcomeMessage />
+                <Message>
+                    <h1 role="heading" aria-level="1">
+                        Boas-vindas ao <br />
+
+                        <b role="img" aria-label="whizDev">
+                            &lt;whizDev/&gt;
+                        </b>
+                    </h1>
+                    <p>Para começar escolha uma categoria</p>
+                </Message>
             </MessageBox>
 
             <OptionsList>
